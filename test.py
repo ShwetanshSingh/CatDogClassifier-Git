9a53e277a4d605451cfa99a1831926fff35cae3a
ShwetanshSingh <contactsinghshwetansh@gmail.com>
2023-09-24 16:14:25 +0530

    added a vscode workspace

---
+++ test.py
@@ -0,0 +1,16 @@
+# from fastai.vision.all import*
+import gradio as gr
+print('hello world')
+# import dill
+# from PIL import Image
+
+# with open('models/pet_breed_50.pkl','rb') as f:
+#     learn = dill.load(f)
+
+# def classify(image):
+#     a,_,_=learn.predict(image)
+#     return a
+
+# demo = gr.Interface(fn=classify,inputs=gr.inputs.Image(),outputs='text',title='Pet Breeds Classifier', description='Classifies 36 dog and cat breeds')
+
+# demo.launch()
